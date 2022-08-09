d37c94ca51f78812db90879acc8b32020e1e0cb8
m.gorshkov <m.gorshkov@skbkontur.ru>
2022-08-09 19:14:14 +0800

    Add exs04

---
+++ exs04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exs04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Введите число: ");
+           int usernumber= Convert.ToInt32(Console.ReadLine());
+           if (usernumber%2==0)
+           {
+           Console.WriteLine("Число четное");
+          
+           }
+           else
+           {
+            Console.WriteLine("Число нечетное");
+            
+           }
+        }
+    }
+}
